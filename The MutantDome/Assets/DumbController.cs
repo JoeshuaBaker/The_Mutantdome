68f35cc00d39985dedfdc44558a9668e5a5e3e14
Jesh <idyliawritehorse@gmail.com>
2018-03-02 17:42:49 -0800

    a commit with the level editor and tile engine implemented along with a simple enemy

---
+++ The MutantDome/Assets/DumbController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DumbController : MonoBehaviour {
+
+	Rigidbody2D rb;
+	Vector2 velocity = new Vector2();
+	public float speed = 10;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		//var x = Input.GetAxis("Horizontal") * Time.deltaTime * 200.0f;
+		//var z = Input.GetAxis("Vertical") * Time.deltaTime * 200.0f;
+
+		if (Input.GetKey (KeyCode.RightArrow))
+			velocity.x = speed;
+		else if (Input.GetKey (KeyCode.LeftArrow))
+			velocity.x = speed * -1;
+		else
+			velocity.x = 0;
+
+		if (Input.GetKey (KeyCode.UpArrow))
+			velocity.y = speed;
+		else if (Input.GetKey (KeyCode.DownArrow))
+			velocity.y = -1 * speed;
+		else
+			velocity.y = 0;
+
+		rb.velocity = velocity;
+	}
+}
---
+++ The MutantDome/Assets/SimpleAnimate.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleAnimate : MonoBehaviour {
+	//public vars to be changed in the editor
+	public bool loop;				//bool that controls if animator loops
+	public float frameSeconds = 1;	//how often to change frames. Defaults to 1 second.
+	public string location;			//File path to the folder where sprite frames are located.
+
+	//private vars to be used within the class
+	private SpriteRenderer spr;		//SpriteRenderer reference on this object
+	private Sprite[] sprites;		//Array of sprites, loaded from filepath provided above
+	private int frame = 0;			//counter for which frame of the sprites we are on
+	private float deltaTime = 0;	//deltaTime represents time between game tick updates.
+
+	// Use this for initialization
+	void Start () {
+		//initialize the sprite renderer
+		spr = GetComponent<SpriteRenderer> ();
+
+		//load sprites from filePath. Will load the entire folder.
+		sprites = Resources.LoadAll<Sprite> (location);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//increment deltaTime
+		deltaTime += Time.deltaTime;
+
+		//if enough time has passed to advance the frame...
+		while (deltaTime >= frameSeconds) {
+			//subtract our frameSeconds to begin tracking again from near zero
+			deltaTime -= frameSeconds;
+
+			//move frame counter 1 forward
+			frame++;
+
+			if (loop) {
+				frame %= sprites.Length;	//if we loop, go back to beginning
+			} else if (frame >= sprites.Length) {
+				frame = sprites.Length - 1;	//else, sit on last frame forever
+			}
+
+			//index into sprites array using the frame counter, and set the spriteRenderer to that sprite
+			spr.sprite = sprites [frame];
+		}
+
+
+	}
+}
